389d033656f05602fd47ba2e1af9d76ceb89c8af
nikshingadiya <shingadiyanikhil1634@gmail.com>
2020-08-13 18:11:04 +0530

    Distribution_Curve

--- Dash/first.py
+++ Dash/first.py
@@ -9,8 +9,8 @@ from dash.dependencies import Input, Output
 import  numpy as np
 import  matplotlib.pylab as plt
 import  plotly.graph_objs as go
-
-import plotly.offline as pyo
+import plotly.figure_factory as ff
+import seaborn as sns
 # df = pd.read_csv('C:/Users/nikhil/Documents/GitHub/jupyter/data/terrorismData.csv').dropna()
 #
 # pd.set_option("display.max_columns", 20)
@@ -49,17 +49,32 @@ app.layout = html.Div(children=[
     ),
     html.Div(["Input-data: ",
               dcc.Input(id='data-point', value=10, type="number")]),
+    html.Div([
+        "Input Mean:",
+        dcc.Input(id="input_Mean",value=0,type="number"),
+        html.Br(),
+        "Input Standard_Deviation:",
+         dcc.Input(id="input_std",value=1,type="number")
+
+
+    ]),
+    dcc.Graph(id="Normal_Dist"),
+
 html.Div(id='my-output')
 ])
 ##############################################################################
 @app.callback(
     [Output(component_id='my-output', component_property='children'),
      Output(component_id='numpy-fig', component_property='figure'),
-     Output(component_id='example-graph', component_property='figure')],
+     Output(component_id='example-graph', component_property='figure'),
+     Output(component_id='Normal_Dist',component_property='figure')],
     [Input(component_id='my-input', component_property='value'),
-     Input(component_id="data-point", component_property='value')]
+     Input(component_id="data-point", component_property='value'),
+     Input(component_id="input_Mean",component_property="value"),
+     Input(component_id="input_std",component_property="value")]
 )
-def update_output_div(input_value,input_value2=10):
+def update_output_div(input_value, input_value2=10, input_value3=0, input_value4=1):
+
     # fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
     data=[go.Scatter(x=df['Fruit'],y=df["Amount"],mode='markers')]
     layout=go.Layout(title="fruit_amount",xaxis={'title':'Fruit'},yaxis={'title':'Amount'})
@@ -74,8 +89,17 @@ def update_output_div(input_value,input_value2=10):
                       )]
     layout1=go.Layout(title="numpy-fig",xaxis=dict(title="x_value"),yaxis=dict(title="y_value"), height=700)
     fig1=go.Figure(data1,layout1)
-    fig1.update_layout(transition_duration=500)
-    return 'Output: {}'.format(input_value),fig1,fig
+    fig1.update_layout(transition_duration=400)
+
+    # data2=np.random.normal(input_value3,input_value4,100)
+    # group_labels = ['distplot']
+    np.random.seed(1)
+
+    x = np.random.normal(input_value3,input_value4,100)
+    hist_data = [x]
+    group_labels = ['distplot']  # name of the dataset
+    fig3 = ff.create_distplot(hist_data, group_labels)
+    return 'Output: {}'.format(input_value),fig1,fig,fig3
 
 
 
